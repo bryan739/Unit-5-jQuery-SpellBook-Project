f1e0206aaec8aac3b88e11cc1f23776792ea42de
bryan739 <48761432+bryan739@users.noreply.github.com>
2019-03-20 10:05:07 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,25 @@
+//Spell #1
+
+$("#disappear").click(function(){
+    $("img").fadeOut();
+});
+
+
+
+//Spell #2
+
+$("#up").click(function(){
+    $("img").slideUp();
+ 
+});
+
+
+
+//Spell #3
+
+$("#toggle").click(function(){
+    $("img").toggle();
+});
+
+
+
